b611722af99c3e654931542ce6e1061001bb01c1
noff04 <80936147+noff04@users.noreply.github.com>
2022-10-17 09:54:54 +1000

    Formatted state=2

--- src/testing_inverse_kinematics/src/new_Motor_angle_publisher.py
+++ src/testing_inverse_kinematics/src/new_Motor_angle_publisher.py
@@ -103,91 +103,37 @@ def inverse_kinematics(pose: Pose) -> JointState:
 
     #'''
     elif state == 2:
-        print('hello')
         desired_x = pose.position.x
         desired_y = pose.position.y
         desired_z = pose.position.z # elevation
-        desired_z=-0.03
-        #desired_x = 0.15
-        #desired_y = 0
-        #desired_z = 0.1
+
         desired_end_angle = -10 * (np.pi/180)
         theta1 = np.arctan2(desired_y,desired_x)
 
         desired_distance = np.sqrt(desired_x**2 + desired_y**2)
         offset = 0.015
-        desired_distance_actual = desired_distance - L4*np.cos(desired_end_angle) - offset*np.sin(desired_end_angle)  #- np.sqrt(robot_origin[0]**2 + robot_origin[1]**2)
+        desired_distance_actual = desired_distance - L4*np.cos(desired_end_angle) - offset*np.sin(desired_end_angle)
         desired_elevation_actual = desired_z - L4*np.sin(desired_end_angle) - L1 - offset*np.cos(desired_end_angle)  
 
-        #'''
-
-        '''
-        desired_x_actual = desired_x - L4*np.cos(desired_end_angle)*np.cos(theta1) - robot_origin[0]
-        desired_y_actual = desired_y - L4*np.cos(desired_end_angle)*np.sin(theta1) - robot_origin[1]
-        desired_z_actual = desired_z - L4*np.sin(desired_end_angle) - robot_origin[2]
-
-        desired_distance_actual = np.sqrt(desired_x_actual**2 + desired_y_actual**2)
-        desired_elevation_actual = desired_z_actual
-        #'''
-
-        # using lecture slides, treat as 2d problem
         plus_or_minus = -1
         costheta3 = (desired_distance_actual**2 + desired_elevation_actual**2 - L2**2 - L3**2) / (2*L2*L3)
         theta3 = m.atan2(plus_or_minus*m.sqrt(1 - costheta3**2),costheta3)
         theta2 = m.atan2(desired_elevation_actual,desired_distance_actual) - np.arctan2(L2*np.sin(theta3),L2 + L3*np.cos(theta3))
-
-        theta2_abs = theta2
-        theta3_abs = theta2 + theta3
-        theta4_abs = desired_end_angle
+        
         theta4 =  desired_end_angle - theta2 - theta3
 
-
-
-        '''
-        # FIND THE LEAST FACTOR ANGLE
-
-        if theta1_abs > 0:
-            theta1_abs = theta1_abs%(2*np.pi)
-            if theta1_abs > np.pi:
-                theta1_abs = -1*(2*np.pi - theta1_abs)
-        else:
-            theta1_abs = (1*theta1_abs)%(-2*np.pi)
-
-        if theta2_relative > 0:
-            theta2_relative = theta2_relative%(2*np.pi)
-            if theta2_relative > np.pi:
-                theta2_relative = -1*(2*np.pi - theta2_relative)
-        else:
-            theta2_relative = (theta2_relative)%(-2*np.pi)
-            if theta2_relative < -np.pi:
-                theta2_relative = 1*(2*np.pi - theta2_relative)
-
-        if theta3_relative > 0:
-            theta3_relative = (theta3_relative) % (2*np.pi)
-            if theta3_relative > np.pi:
-                theta3_relative = -1*(2*np.pi - theta3_relative)
-        else:
-            theta3_relative = (theta3_relative)%(-2*np.pi)
-            if theta3_relative < -np.pi:
-                theta3_relative = 1*(2*np.pi + theta3_relative)
-
-        #'''
-
-        # CORRECT THETA1 TO THE VERTICAL
+        # CORRECT THETA2 TO THE VERTICAL
         if theta2_abs > np.pi/2: # > 90deg
             theta2_abs = theta2_abs - np.pi/2
         elif theta2_abs < np.pi/2:
             theta2_abs = np.pi/2 - theta2_abs
         
-        theta2=-theta2_abs
+        # Account for Dynamixel rotation directions
+        theta2=-theta2
         theta4=-theta4
 
-
-
         msg = JointState(
-                # Set header with current time
                 header=Header(stamp=rospy.Time.now()),
-                # Specify joint names (see `controller_config.yaml` under `dynamixel_interface/config`)
                 name=['joint_1','joint_2', 'joint_3', 'joint_4']
             )
 
@@ -196,7 +142,7 @@ def inverse_kinematics(pose: Pose) -> JointState:
             theta2,
             theta3,
             theta4
-            ]   # THIS DOESNT INCLUDE HORIZONTAL  
+            ]
         i=0
         while (i < 30):
             pub_joint.publish(msg)
