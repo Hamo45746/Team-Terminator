f686eed947905324d49533ef8a81bb7599766290
noff04 <80936147+noff04@users.noreply.github.com>
2022-10-24 11:37:31 +1000

    Delete src/metr4202_w7_prac directory

--- src/metr4202_w7_prac/src/joint_states_publisher.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import rospy 
-# use geometry_msgs/Pose for describing the end efrector pose
-
-iinverse kinematics node rthat subscribes toi pose "desired pose" which publhes the desired joint states which is what dynamixel reads. 
-uploaded to wk7 prac reo
